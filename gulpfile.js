19efd78adaf6770c6f03aed515e61a3bc09bc822
Radovan Buljic <buljic.radovan@gmail.com>
2022-03-15 13:07:08 +0100

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp"),
-  sass = require("gulp-sass")(require("sass"));
+  sass = require("gulp-sass")(require("sass")),
+  autoprefixer = require("gulp-autoprefixer");
+const sourceMaps = require("gulp-sourcemaps");
 
 const styleSRC = "./src/sass/**/*.scss";
 const styleDist = "./dist/css";
@@ -7,7 +9,15 @@ const styleDist = "./dist/css";
 function css() {
   return gulp
     .src(styleSRC)
+    .pipe(sourceMaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(sourceMaps.write("./"))
     .pipe(gulp.dest(styleDist));
 }
 
