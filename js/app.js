57139aa7338095c5dc66b20d71ffa087f09ef06b
Radovan Buljic <buljic.radovan@gmail.com>
2022-03-22 00:17:51 +0100

    banner and hero

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// ------------------------------------------
+// HUMBURGER
+// ------------------------------------------
+const ham = document.querySelector(".humburger");
+const headerManu = document.querySelector(".header_manu__links");
+const headerButtons = document.querySelector(".header_manu__btns");
+
+console.log(ham);
+
+ham.addEventListener("click", function (e) {
+  e.stopPropagation();
+  ham.classList.toggle("active");
+  if (ham.classList.contains("active")) {
+    headerManu.style.display = "block";
+  } else {
+    headerManu.style.display = "none";
+  }
+});
